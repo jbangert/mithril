782e5935d847ffdccea640b211d3604ec820122c
Julian Bangert <jbangert@acm.org>
2013-05-30 10:34:39 -0400

    Reorganization

---
+++ elf-toolkit/lib/main.rb
@@ -0,0 +1,11 @@
+require_relative 'parser'
+require_relative 'writer'
+
+$parse = Elf::Parser.from_file (ARGV[0] || "/bin/ls")
+Elf::Writer::Writer.to_file("/tmp/tst",$parse)
+`chmod +x /tmp/tst`
+binding.pry
+
+#pp parse # .instance_variables
+##TODO: Do enums as custom records.
+
